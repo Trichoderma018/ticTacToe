1acc36a55f135e9587cbee6dde65b2a5d8cee706
juanpa <juan.paq72@mail.com>
2025-02-11 15:57:20 -0600

    variables traidas por id en js

---
+++ script.js
@@ -0,0 +1,15 @@
+let item0 = document.getElementById("item0")
+let item1 = document.getElementById("item1")
+let item2 = document.getElementById("item2")
+let item3 = document.getElementById("item3")
+let item4 = document.getElementById("item4")
+let item5 = document.getElementById("item5")
+let item6 = document.getElementById("item6")
+let item7 = document.getElementById("item7")
+let item8 = document.getElementById("item8")
+
+cuadriculas = [item0, item1, item2, item3, item4, item5, item6, item7, item8]
+
+array.forEach(element => {
+    
+});
