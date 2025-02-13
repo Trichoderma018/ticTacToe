940e14dc496b622899ddba4129eb85474c0118a3
Juan Quesada <juan.paq72@gmail.com>
2025-02-13 17:24:40 -0600

    Inicie la funcion de ganador, falta logica

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ let reset = document.getElementById("reinicio")
 cuadriculas = [item0, item1, item2, item3, item4, item5, item6, item7, item8]
 //let jugadorActual = "X"
 const turnos = ["X","O"]
-
+let matriz = [[0,1,2],[3,4,5],[6,7,8]]
+//console.log(matriz[1][1]);
 jugador() //Es la manera en que los usuarios marcan ´x´ o ´O´
 reset.addEventListener("click",reiniciar) //Reinicia el juego limpiando el tablero
 
@@ -29,11 +30,33 @@ function jugador() {
 function maquina() {
     const filtro = cuadriculas.filter((item) => item.textContent === "") //Se filtran las cuadriculas vacias
     const aleatorio = Math.floor(Math.random() * filtro.length) //se realiza un numero aleatorio segun la cantiadad de cuadriculas vacias
-    filtro[aleatorio].innerHTML = turnos[1] //Despues 
+    setTimeout(() => {
+        filtro[aleatorio].innerHTML = turnos[1] //Despues 
+    }, 1500);
 }
-
 function reiniciar () {
     cuadriculas.forEach((element) => {
         element.innerHTML = ""
     })
+}
+function ganador() {
+    for (let i = 0; i < matriz.length; i++) {
+        
+        for (let j = 0; j < matriz.length; j++) {
+            //console.log(matriz[index][j])
+            if ((matriz[i][j]) !== "" && matriz[0][i] === matriz[1][i] && matriz[1][i] === matriz[2][i]) {
+                alert(`has ganado`)
+            } else {
+                if (condition) {
+                    
+                } else {
+                    if (condition) {
+                        
+                    }
+                }
+            }
+            
+        }
+    }
+    
 }
